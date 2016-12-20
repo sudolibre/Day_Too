98d95645c51f046d7baf75164c32bfeafd362497
sudolibre <optout.breaker@gmail.com>
2016-12-20 16:31:01 -0500

    submission candidate

---
+++ Day_Too/Journal.swift
@@ -0,0 +1,14 @@
+//
+//  Entry.swift
+//  Day_Too
+//
+//  Created by Jonathon Day on 12/20/16.
+//  Copyright © 2016 dayj. All rights reserved.
+//
+
+import Foundation
+
+struct Entry {
+    
+    addNewEntry
+}
---
+++ Day_Too/Main.swift
@@ -0,0 +1,9 @@
+//
+//  Main.swift
+//  Day_Too
+//
+//  Created by Jonathon Day on 12/20/16.
+//  Copyright © 2016 dayj. All rights reserved.
+//
+
+import Foundation
